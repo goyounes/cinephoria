b35d12f9f2688c3ab2ad920f74fba7525df0acec
goyounes <younes.sahar.ing@gmail.com>
2025-06-01 22:16:42 +0200

    fetched movies and displayed them in movies.jsx page

--- client/src/pages/Movies.jsx
+++ client/src/pages/Movies.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 const Movies = () => {
+  const [movies, setMovies] = useState([])
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/movies');
+        setMovies(response.data);
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    };
+    fetchMovies();
+  } , [])
   return (
-    <div>Movies</div>
+    <div>
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h2>{movie.title}</h2>
+          <p>{movie.description}</p>
+          <p>Release Date: {movie.releaseDate}</p>
+          <p>Rating: {movie.rating}</p>
+        </div>
+      ))}
+    </div>
   )
+
 }
+  
 
 export default Movies
