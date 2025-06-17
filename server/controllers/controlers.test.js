0202f6abca8c4ed6341cf3f02439de20cb96b4dc
goyounes <younes.sahar.ing@gmail.com>
2025-06-18 00:52:46 +0200

    added test for controlers (review is manual checking)

---
+++ server/controllers/controlers.test.js
@@ -0,0 +1,27 @@
+import { getCinemas, getRooms, getSeats } from "./cinemas.js";
+import { getMessages } from "./messages.js";
+import { getGenres, getMovies, getMoviesWithGenres } from "./movies.js";
+import { getAllScreenings, getScreeningQualities, getScreenings, getUpcomingScreenings } from "./screenings.js";
+import { getCheckoutInfo, getTickets } from "./tickets.js";
+import { getUser, getUsers } from "./users.js";
+
+// console.table(await getCinemas());
+// console.table(await getRooms());
+// console.table(await getSeats());
+
+// console.table(await getMessages());
+// console.table(await getMovies());
+// console.table(await getMoviesWithGenres());
+// console.table(await getGenres());
+
+// console.table(await getAllScreenings());
+// console.table(await getUpcomingScreenings());
+// console.table(await getScreenings());
+// console.table(await getScreeningQualities());
+
+// console.table(await getTickets());
+// console.table(await getCheckoutInfo(1));
+
+// console.table(await getUsers());
+// console.table(await getUser(1));
+
