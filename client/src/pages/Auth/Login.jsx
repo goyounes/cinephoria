8a44ed903b6ef53a7cad60c49c7bf47156f5fe25
goyounes <younes.sahar.ing@gmail.com>
2025-07-10 17:05:36 +0200

    added Login logic

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import axios from 'axios';
 import LoginIcon from '@mui/icons-material/Login';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Container, Typography, Stack, TextField, Button, FormControl, InputLabel, Select, MenuItem, Card, CardContent} from '@mui/material';
 const Login = () => {
+  const API_URL = "http://localhost:8080";
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    // password: '',
+  });
+
+  const handleChange = (e) => {
+    // console.log("e.target: ", e.target);
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(`${API_URL}/auth/login`)
+      console.log(formData)
+
+      const response = await axios.post(`${API_URL}/auth/login`, formData);
+      const result = response.data
+      console.log("response of login: ");
+      console.log(response)
+      alert('Login successful! \nWelcome ' + formData.username);
+      // setFormData({email: '',password: '',username: '',firstName: '',lastName: '',});
+      navigate('/home'); // Redirect to the users page
+    } catch (err) {
+      alert('Failed to add User: ' + err.message);
+    }
+  };
+
    return (
     <Container maxWidth="sm" sx={{ flexGrow: 1 , py:4, display:'flex', alignItems: 'center'}}>
       <Card elevation={2} sx={{flexGrow: 1  }}>
@@ -12,41 +49,38 @@ const Login = () => {
             Login Page
           </Typography>
 
-          <TextField
+          {/* <TextField
             fullWidth
             required
             label="Email"
-            name="user_email"
+            name="email"
             type="email"
             placeholder="example@mail.com"
+            onChange={handleChange}
+            value={formData.email}
+          /> */}
+          <TextField
+            fullWidth
+            required
+            label="Username"
+            name="username"
+            placeholder="Please enter your username"
+            onChange={handleChange}
+            value={formData.username}
           />
 
           <TextField
             fullWidth
             required
             label="Password"
-            name="user_password"
+            name="password"
             type="password"
-            placeholder="8 characters minimum"
+            placeholder="Please enter your password"
+            onChange={handleChange}
+            value={formData.password}
           />
 
-          <TextField
-            fullWidth
-            required
-            label="Username"
-            name="user_name"
-            placeholder="Unique Username"
-          />
-
-          <FormControl fullWidth required>
-            <InputLabel id="role-label">Role</InputLabel>
-            <Select labelId="role-label" name="role_id" label="Role">
-              <MenuItem value={1}>User</MenuItem>
-              <MenuItem value={2}>Employee</MenuItem>
-              <MenuItem value={3}>Admin</MenuItem>
-            </Select>
-          </FormControl>
-          <Button variant="contained" color="primary" type="submit" startIcon={<LoginIcon />}>
+          <Button variant="contained" color="primary" onClick={handleSubmit} startIcon={<LoginIcon />}>
             Login
           </Button>
         </Stack>
