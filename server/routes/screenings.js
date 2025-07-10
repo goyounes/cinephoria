cb0709ad57b3f7c56e0ad92acf961449eb4eff0e
goyounes <younes.sahar.ing@gmail.com>
2025-07-10 15:33:02 +0200

    GetScreenings is used instead of queering old API

--- server/routes/screenings.js
+++ server/routes/screenings.js
@@ -1,26 +1,35 @@
 import { Router } from 'express';
 const router = Router();
 import axios from 'axios';
-
+import {getAllScreenings} from '../controllers/screenings.js'; 
 const DB_API_URL = "http://localhost:5000/api/v1"
 
 
-router.get("/",async (req,res,next) => {
-    const cinema_id = req.query.cinema_id || null;
-    const movie_id = req.query.movie_id || null;
-    try {
-        const url = new URL(DB_API_URL+"/screenings");
-        if (cinema_id) url.searchParams.routerend("cinema_id", cinema_id);
-        if (movie_id) url.searchParams.routerend("movie_id", movie_id);
+// router.get("/",async (req,res,next) => {
+//     const cinema_id = req.query.cinema_id || null;
+//     const movie_id = req.query.movie_id || null;
+//     try {
+//         const url = new URL(DB_API_URL+"/screenings");
+//         if (cinema_id) url.searchParams.routerend("cinema_id", cinema_id);
+//         if (movie_id) url.searchParams.routerend("movie_id", movie_id);
 
-        const response = await axios.get(url,{headers:{'X-Requested-By': 'backend-server'}})
-        const screenings = response.data
-        // res.status(200).render("pages/screenings.ejs",{screenings})
-        res.status(200).json(response.data)
+//         const response = await axios.get(url,{headers:{'X-Requested-By': 'backend-server'}})
+//         const screenings = response.data
+//         // res.status(200).render("pages/screenings.ejs",{screenings})
+//         res.status(200).json(response.data)
+//     } catch (error) {
+//         next(error)
+//     }
+// })
+router.get("/",async (req,res,next) => {
+    try {
+        const screenings = await getAllScreenings()
+        res.status(200).json(screenings)
     } catch (error) {
         next(error)
     }
 })
+
 
 router.get('/create', (req, res,next) => {
     // res.sendFile("/static/create_screening.html",{root:"."})
