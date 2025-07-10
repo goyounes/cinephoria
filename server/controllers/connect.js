9f342462d17bbb8f449a3e96ad897a32dca617ab
goyounes <younes.sahar.ing@gmail.com>
2025-07-10 22:20:17 +0200

    temporary fix, i need to figure out why env not working later

--- server/controllers/connect.js
+++ server/controllers/connect.js
@@ -3,14 +3,15 @@ import dotenv from "dotenv";
 dotenv.config({ path: './server/.env' });
 
 export const pool = mysql.createPool({
-    host : process.env.MYSQL_HOST,
-    user : process.env.MYSQL_USER,
-    password : process.env.MYSQL_PASSWORD,
-    database : process.env.MYSQL_DATABASE
+    host : process.env.MYSQL_HOST || "localhost",
+    user : process.env.MYSQL_USER || "root",
+    password : process.env.MYSQL_PASSWORD || "5599",
+    database : process.env.MYSQL_DATABASE || "cinephoria",
 }).promise()
 
+console.log("Connected to MySQL database:", process.env.MYSQL_DATABASE);
 
 
-// console.log(pool)
-// async function testConnection() {    try {        const connection = await pool.getConnection();        console.log("Connected to MySQL!");        connection.release();     } catch (error) {        console.error("Connection failed:", error);    }}
-// testConnection();
+console.log(pool)
+async function testConnection() {    try {        const connection = await pool.getConnection();        console.log("Connected to MySQL!");        connection.release();     } catch (error) {        console.error("Connection failed:", error);    }}
+testConnection();
