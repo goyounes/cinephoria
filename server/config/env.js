1d2476b32ad537f0dd38f3540fef2dc66ef8bd94
goyounes <younes.sahar.ing@gmail.com>
2025-07-11 14:16:38 +0200

    removed log statments

--- server/config/env.js
+++ server/config/env.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
-process.env.DOTENV_CONFIG_DEBUG = 'true';
+// process.env.DOTENV_CONFIG_DEBUG = 'true';
 
 dotenv.config({ path: './.env' });
 
-console.log("Younes : Server: Environment variables loaded successfully!");
+// console.log("Younes : Server: Environment variables loaded successfully!");
 // console.log(process.env)
