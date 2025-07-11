1db272343f88f1a19c2e2db4ef528f90a430435d
goyounes <younes.sahar.ing@gmail.com>
2025-07-11 11:02:25 +0200

    image recived in the backend is uploaded to the S3 bucket

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -1,7 +1,37 @@
 import { Router } from 'express';
 const router = Router();
+
 import axios from 'axios';
+import multer from 'multer';
+import crypto from 'crypto';
+
 import { addMovie, getMovies, getMoviesWithGenres } from '../controllers/movies.js';
+
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const randomImageName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex') ; // Generate a random name for the image
+
+const bcuketName = process.env.S3_BUCKET_NAME;
+const bucketRegion = process.env.S3_BUCKET_REGION;
+const accesKey = process.env.S3_BUCKET_ACCES_KEY;
+const secretAcces_key = process.env.S3_BUCKET_SECRET_ACCES_KEY;
+// console.log(bcuketName, bucketRegion, accesKey, secretAcces_key);   
+
+const s3 = new S3Client({
+    credentials: {
+        accessKeyId: accesKey,  
+        secretAccessKey: secretAcces_key,
+    },
+    region: bucketRegion,
+})
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+upload.single('poster_img') // 'poster_img' is the field name in the form
 
 const DB_API_URL = "http://localhost:5000/api/v1"
 
@@ -16,11 +46,28 @@ router.get("/",async (req,res,next) => {
         next(error)
     }
 })
-
-router.post("/",async (req,res,next) => {
+// upload.single('file'), 
+router.post("/", upload.single('poster_img'), async (req,res,next) => {
     try {
-        const movies = await addMovie()
-        res.status(200).json(movies)
+        console.log("req.body : ",req.body); 
+        console.log("req.file : ",req.file); 
+        console.log("req.file.buffer",req.file.buffer);
+        const imageName = randomImageName();
+        const params = {
+            Bucket: bcuketName,
+            Key: imageName, 
+            Body: req.file.buffer, 
+            ContentType: req.file.mimetype, // e.g., 'image/png'
+        }
+
+        const command = new PutObjectCommand(params)
+
+        await s3.send(command)
+
+        // console.log(req.body); // other form fields
+        // const movieData = {}
+        // const response = await addMovie()
+        res.status(200).json("s3 bucket upload successful, movie added to database");
     } catch (error) {
         next(error)
     }
