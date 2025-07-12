d793f50a00fe15eb888a209a05f5bad8e50ea259
goyounes <younes.sahar.ing@gmail.com>
2025-07-12 22:58:46 +0200

    moviesTable no longer refactored in it's own component

--- client/src/pages/Movies/MoviesTable.jsx
+++ client/src/pages/Movies/MoviesTable.jsx
@@ -3,45 +3,7 @@ import { Link } from "react-router-dom";
 const MovieTable = (props) => {
     const { movies } = props;
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Poster</th>
-          <th>Title</th>
-          <th>Description</th>
-          <th>Age Rating</th>
-          <th>Team Pick</th>
-          <th>Score</th>
-          <th>Length</th>
-        </tr>
-      </thead>
-      <tbody>
-        {movies.map((movie) => (
-          <tr key={movie.movie_id} className={movie.is_team_pick ? "highlight" : ""}>
-            <td>
-              {/* {console.log(movie.imageUrl)} */}
-              {movie.imageUrl ? (
-                <img
-                  src={movie.imageUrl} 
-                  alt={`Poster for ${movie.title}`}
-                  style={{ width: "80px", height: "auto", borderRadius: "5px" }}
-                />
-              ) : (
-                "N/A"
-              )}
-            </td>
-            <td>
-              <Link to={`/movies/${movie.movie_id}`}>{movie.title}</Link>
-            </td>
-            <td>{movie.description}</td>
-            <td>{movie.age_rating}</td>
-            <td>{movie.is_team_pick ? "Yes" : "No"}</td>
-            <td>{movie.score}</td>
-            <td>{movie.length}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div>empty</div>
   );
 };
 
