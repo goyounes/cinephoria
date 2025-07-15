a33ae2efbd57e2ab94d70922fc429735c81ba8cf
goyounes <younes.sahar.ing@gmail.com>
2025-07-15 15:52:44 +0200

    fixed controled input bug

--- client/src/components/BasicDatePicker.jsx
+++ client/src/components/BasicDatePicker.jsx
@@ -1,19 +1,16 @@
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-
-import { TextField } from '@mui/material';
 import { useState } from 'react';
 
-const BasicDatePicker = () => {
-    const [value,setValue] = useState(null)
-    
+const BasicDatePicker = ({value,onChange}) => {
+    // const [value,setValue] = useState(null)
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DatePicker
             label="Basic date picker"
             value={value}
-            onChange={setValue}
+            onChange={onChange}
         />
     </LocalizationProvider>
   );
