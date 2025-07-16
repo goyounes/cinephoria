988f7ed232128e2c7b19136e0e203fe2615a15bd
goyounes <younes.sahar.ing@gmail.com>
2025-07-16 02:50:47 +0200

    added specific verifying routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,20 @@
 import { Router } from 'express';
 const router = Router();
 
-import { register, login, logout, verifyUserJWT } from '../controllers/auth.js';
+import { register, login, logout, verifyUserJWT , verifyEmployeeJWT , verifyAdminJWT} from '../controllers/auth.js';
 router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
 router.post('/verify', verifyUserJWT, (req, res) => {
+  res.status(200).json({ message: 'Token valid', user: req.user });
+});
+router.post('/verify/user', verifyUserJWT, (req, res) => {
+  res.status(200).json({ message: 'Token valid', user: req.user });
+});
+router.post('/verify/employee', verifyEmployeeJWT, (req, res) => {
+  res.status(200).json({ message: 'Token valid', user: req.user });
+});
+router.post('/verify/admin', verifyAdminJWT, (req, res) => {
   res.status(200).json({ message: 'Token valid', user: req.user });
 });
  
