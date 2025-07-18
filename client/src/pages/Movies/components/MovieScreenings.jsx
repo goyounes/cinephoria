1f30fa038f49ab3fff92f2998d9fa7a607e1e4bf
goyounes <younes.sahar.ing@gmail.com>
2025-07-18 16:49:05 +0200

    rafactored code

--- client/src/pages/Movies/components/MovieScreenings.jsx
+++ client/src/pages/Movies/components/MovieScreenings.jsx
@@ -8,7 +8,7 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import { Stack, Button, Typography, IconButton, Card, CardContent } from "@mui/material";
 
-import ScreeningButton from "./ScreeningButton";
+import ScreeningsDisplay from "./ScreeningsDisplay";
 
 const MovieScreenings = ({ movieId, nbrOfTickets = 0}) => {
   const [screenings, setScreenings] = useState([]);
@@ -171,14 +171,12 @@ const MovieScreenings = ({ movieId, nbrOfTickets = 0}) => {
 
             return (
               <>
-                <Typography variant="h6" gutterBottom>
-                  Screenings for {selectedDate}
-                </Typography>
+                <Typography variant="h6" gutterBottom> Screenings for {selectedDate}</Typography>
 
                 {screeningsForDate.length === 0 ? (
                   <Typography>No screenings available.</Typography>
                 ) : (
-                  <ScreeningsList screeningsByLocation={screeningsForDate} nbrOfTickets={nbrOfTickets} />
+                  <ScreeningsDisplay screeningsByLocation={screeningsForDate} nbrOfTickets={nbrOfTickets} />
                 )}
               </>
             );
@@ -189,48 +187,6 @@ const MovieScreenings = ({ movieId, nbrOfTickets = 0}) => {
   );
 };
 
-const ScreeningsList = ({ screeningsByLocation, nbrOfTickets }) => {
-  if (!screeningsByLocation) return null;
 
-  return (
-    <Stack spacing={1}>
-      {Object.entries(screeningsByLocation).map(([cinemaId, cinemaData]) => {
-        if (typeof cinemaData !== "object" || !cinemaData.cinema_id) return null;
-
-        return (
-          <Stack id="Cinema_Screenings" key={cinemaId}>
-            <Typography variant="subtitle1" gutterBottom sx={{ fontWeight: "bold" }}>
-              Cinema: {cinemaData.cinema_name}
-            </Typography>
-
-            <Stack spacing={2}>
-            {Object.entries(cinemaData).map(([roomId, roomData]) => {
-                if (!roomData?.screenings) return null;
-
-                const visibleScreenings = roomData.screenings.filter(
-                  (screening) => screening.seats_left >= nbrOfTickets
-                );
-
-                if (visibleScreenings.length === 0) return null;
-
-                return (
-                  <Stack id="Room_Screenings" key={roomId} direction="row" sx={{ flexWrap: "wrap", gap: 1 }}>
-                    {visibleScreenings.map((screening) => (
-                      <ScreeningButton
-                        key={screening.screening_id}
-                        screening={screening}
-                        room_name={roomData.room_name}
-                      />
-                    ))}
-                  </Stack>
-                );
-              })}
-            </Stack>
-          </Stack>
-        );
-      })}
-    </Stack>
-  );
-};
 
 export default MovieScreenings;
---
+++ client/src/pages/Movies/components/ScreeningsDisplay.jsx
@@ -0,0 +1,49 @@
+import { Stack, Typography } from '@mui/material';
+import ScreeningButton from './ScreeningButton';
+
+
+const ScreeningsDisplay = ({ screeningsByLocation, nbrOfTickets }) => {
+  if (!screeningsByLocation) return null;
+
+  return (
+    <Stack spacing={1}>
+      {Object.entries(screeningsByLocation).map(([cinemaId, cinemaData]) => {
+        if (typeof cinemaData !== "object" || !cinemaData.cinema_id) return null;
+
+        return (
+          <Stack id="Cinema_Screenings" key={cinemaId}>
+            <Typography variant="subtitle1" gutterBottom sx={{ fontWeight: "bold" }}>
+              Cinema: {cinemaData.cinema_name}
+            </Typography>
+
+            <Stack spacing={2}>
+            {Object.entries(cinemaData).map(([roomId, roomData]) => {
+                if (!roomData?.screenings) return null;
+
+                const visibleScreenings = roomData.screenings.filter(
+                  (screening) => screening.seats_left >= nbrOfTickets
+                );
+
+                if (visibleScreenings.length === 0) return null;
+
+                return (
+                  <Stack id="Room_Screenings" key={roomId} direction="row" sx={{ flexWrap: "wrap", gap: 1 }}>
+                    {visibleScreenings.map((screening) => (
+                      <ScreeningButton
+                        key={screening.screening_id}
+                        screening={screening}
+                        room_name={roomData.room_name}
+                      />
+                    ))}
+                  </Stack>
+                );
+              })}
+            </Stack>
+          </Stack>
+        );
+      })}
+    </Stack>
+  );
+};
+
+export default ScreeningsDisplay
