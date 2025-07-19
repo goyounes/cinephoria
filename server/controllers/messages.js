065ab287e157a4e6c1bf1975b0fa9acadcbd2903
goyounes <younes.sahar.ing@gmail.com>
2025-07-19 11:09:46 +0200

    added admin/messages POst endpoint, availble without auth

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -5,15 +5,15 @@ export async function  getMessages(){
     const [result_rows] = await pool.query(q);
     return result_rows
 }
-export async function  addMessage(m){
+export async function  addMessage(message){
     const q = `INSERT INTO messages(message_subject, message_text, message_sender_name, message_sender_email)
                VALUES (?,?,?,?);
               `
     const VALUES = [
-    "message_subject",
-    "message_text.....", 
-    "message_sender_name",
-    "message_sender_email"
+        message.message_subject,
+        message.message_text,
+        message.message_sender_name,
+        message.message_sender_email,
     ]
     const [insertResult] = await pool.query(q,VALUES);
     return insertResult
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 const router = Router();
 import axios from 'axios';
-import { getMessages } from '../controllers/messages.js';
+import { addMessage, getMessages } from '../controllers/messages.js';
 import  { verifyEmployeeJWT} from '../controllers/auth.js';
 
 
@@ -17,6 +17,20 @@ router.get("/messages", verifyEmployeeJWT, async (req,res,next) => {
     try {
         const messages = await getMessages()
         res.status(200).json(messages)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.post("/messages", async (req,res,next) => {
+    try {
+        const result = await addMessage({
+            message_subject : req.body.message_subject, 
+            message_text : req.body.message_text, 
+            message_sender_name : req.body.message_sender_name,
+            message_sender_email : req.body.message_sender_email
+        })
+        res.status(200).json(result)
     } catch (error) {
         next(error)
     }
