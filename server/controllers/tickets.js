d1848800c859f5159f94e505ce49e80dc9b06e1d
goyounes <younes.sahar.ing@gmail.com>
2025-07-22 21:18:54 +0200

    implemented the getRemainingSeats function

--- server/controllers/tickets.js
+++ server/controllers/tickets.js
@@ -23,3 +23,18 @@ export async function  addTicket(t){
     const [insertResult] = await pool.query(q,VALUES);
     return insertResult
 }
+
+export async function getRemainingSeats(screening_id){
+    const q = `
+        SELECT 
+            (COUNT(seats.seat_id) - COUNT(tickets.seat_id)) AS seats_left
+        FROM screenings
+        JOIN rooms ON screenings.room_id = rooms.room_id
+        JOIN seats ON seats.room_id = rooms.room_id AND seats.isDeleted = FALSE
+        LEFT JOIN tickets ON tickets.screening_id = screenings.screening_id AND tickets.seat_id = seats.seat_id
+        WHERE screenings.screening_id = ?
+        GROUP BY screenings.screening_id, rooms.room_capacity
+    ` 
+    const [result_rows] = await pool.query(q,[screening_id]);
+    return result_rows
+}
