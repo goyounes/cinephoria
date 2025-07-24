656bb235f8b18c921ab2bcee6d423f8f5315ce78
goyounes <younes.sahar.ing@gmail.com>
2025-07-24 22:46:01 +0200

    removed logging

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -4,5 +4,4 @@ const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_URL || '/',
 });
 
-console.log("Axios baseUrl", process.env.REACT_APP_API_URL || '/')
 export default axiosInstance;
