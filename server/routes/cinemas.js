bfef9ece5ddc560b0cc457c7aeddfe014fede8c9
goyounes <younes.sahar.ing@gmail.com>
2025-07-30 11:30:57 +0200

    added rooms route

--- server/routes/cinemas.js
+++ server/routes/cinemas.js
@@ -4,8 +4,17 @@ import {getCinemas, getRooms, getSeats} from '../controllers/cinemas.js';
 
 router.get("/", async (req,res,next) => {
     try {
-        const messages = await getCinemas()
-        res.status(200).json(messages)
+        const cinemas = await getCinemas()
+        res.status(200).json(cinemas)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.get("/rooms", async (req,res,next) => {
+    try {
+        const rooms = await getRooms()
+        res.status(200).json(rooms)
     } catch (error) {
         next(error)
     }
