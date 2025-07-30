f9da8077180fcf8093b08ff5468285041c83971b
goyounes <younes.sahar.ing@gmail.com>
2025-07-30 15:09:29 +0200

    added RoomMultiSelect component

---
+++ client/src/pages/components/RoomMultiSelect.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+  Checkbox,
+  ListItemText,
+} from "@mui/material";
+
+const RoomMultiSelect = ({ rooms, selectedRooms, setSelectedRooms }) => {
+  const handleChange = (event) => {
+    setSelectedRooms(event.target.value);
+  };
+
+  return (
+    <FormControl fullWidth>
+      <InputLabel id="room-multi-select-label">Rooms</InputLabel>
+      <Select
+        labelId="room-multi-select-label"
+        multiple
+        value={selectedRooms}
+        onChange={handleChange}
+        renderValue={(selected) =>
+          selected.map((id) => {
+            const room = rooms.find((r) => r.room_id === id);
+            return room ? room.room_name : id;
+          }).join(', ')
+        }
+        label="Rooms"
+      >
+        {rooms.map((room) => (
+          <MenuItem key={room.room_id} value={room.room_id}>
+            <Checkbox checked={selectedRooms.includes(room.room_id)} />
+            <ListItemText primary={room.room_name} />
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default RoomMultiSelect;
