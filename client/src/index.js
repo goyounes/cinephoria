39af30df2432504741da14937e1df92d24642314
goyounes <younes.sahar.ing@gmail.com>
2025-09-07 15:26:50 +0200

    added support for electron app functinality

--- client/src/index.js
+++ client/src/index.js
@@ -2,20 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthProvider';
 import { SnackbarProvider } from './context/SnackbarProvider';
 
-
+// Detect if running in Electron and choose appropriate router
+const isElectron = window.navigator.userAgent.includes('Electron');
+const Router = isElectron ? HashRouter : BrowserRouter;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <SnackbarProvider>
-        <BrowserRouter>
+        <Router>
           <App />
-        </BrowserRouter>
+        </Router>
       </SnackbarProvider>
     </AuthContextProvider>
   </React.StrictMode>
