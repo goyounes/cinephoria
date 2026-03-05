00509a61880592e66e0ed4f16dfb2eee3c703f2e
goyounes <younes.sahar.ing@gmail.com>
2026-03-05 14:03:32 +0100

    removed js files

--- server/utils/randomImageName.js
+++
@@ -1,5 +0,0 @@
-import crypto from 'crypto';
-
-const randomImageName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex') ; // Generate a random name for the image
-
-export default randomImageName;
